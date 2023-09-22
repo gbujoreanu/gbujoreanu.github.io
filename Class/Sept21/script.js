7f6ae938c3eb0dbe8c28ce9705c5f1241102b33b
gbujoreanu <gbujoreanu@gmail.com>
2023-09-22 14:30:29 -0400

    Adding in Class Assignment

---
+++ Class/Sept21/script.js
@@ -0,0 +1,8 @@
+const toggleNav = () => {
+    document.getElementById("nav-items").classList.toggle("hide-small");
+  };
+  
+  window.onload = () => {
+    document.getElementById("hamburger").onclick = toggleNav;
+  };
+  
