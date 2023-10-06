ee44cfa0b8fbb00db106f1beed725260c1bc81ae
gbujoreanu <111786155+gbujoreanu@users.noreply.github.com>
2023-10-06 17:01:27 -0400

    adding Part_1

---
+++ Projects/Part_1/calendar/script.js
@@ -0,0 +1,7 @@
+const toggleNav = () => {
+    document.querySelector(".menu").classList.toggle("active");
+};
+
+window.onload = () => {
+    document.getElementById("menuToggle").onclick = toggleNav;
+};
